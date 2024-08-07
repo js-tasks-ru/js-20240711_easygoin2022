dac1f52d38c2646ae08fe383e2e31cdfd57a63ed
Simon Sklyarov <simvaliant@gmail.com>
2024-08-07 20:33:21 +0300

    fixed

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,7 +1,9 @@
 import SortableTable from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
 
 export default class SortableTableV2 extends SortableTable{
-
+  isSortLocally = true;
+  sortField;
+  sortOrder;
 
   constructor(headersConfig, {
     data = [],
@@ -16,34 +18,50 @@ export default class SortableTableV2 extends SortableTable{
     this.sorted = sorted;
 
     
-    super.render();
+    super.createElement(super.getTable());
     super.sort(this.sorted.id, this.sorted.order);
     this.createListeners();
     
   }
 
+  sort () {
+    if (this.isSortLocally) {
+      this.sortOnClient();
+    } else {
+      this.sortOnServer();
+    }
+  }
+
+  sortOnClient(){
+    super.sort(this.sortField, this.sortOrder);
+  }
+
+  sortOnServer(){}
+
 
   createListeners(){
-    document.addEventListener('click', this.handleDocumentClick, true);
+    document.addEventListener('pointerdown', this.handleElementPointerdown, true);
   }
 
   destroyListeners(){
-    document.removeEventListener('click', this.handleDocumentClick);
+    document.removeEventListener('pointerdown', this.handleElementPointerdown);
   }
 
 
-  handleDocumentClick(event) {
-    const target = event.target.closest('[data-id]');
-    
-    if (target) {
-      const id = target.dataset.id;
-      
-      if (id) {
-        this.sorted.id = id;
-        this.sorted.order = this.sorted.order === 'asc' ? 'desc' : 'asc'; 
-        super.sort(this.sorted.id, this.sorted.order);
-      }
+  handleElementPointerdown = (event) => {
+    const target = event.target.closest('.sortable-table__cell[data-id]');
+
+    if (!target) {
+      return;
     }
+
+    const id = target.dataset.id;
+    const sortOrder = target.dataset.order === `desc` ? `desc` : `desc`;
+
+    this.sorted.id = id;
+    this.sorted.order = sortOrder; 
+
+    super.sort(this.sorted.id, this.sorted.order);
   }
 
   destroy(){
